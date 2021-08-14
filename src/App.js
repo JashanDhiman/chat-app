3993fea1b15e718acd8ba29f5777232d5490117e
jashan <jashandhiman.hulk@gmail.com>
2021-08-14 18:11:51 +0530

    added signin page with auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/home';
+import SignIn from './pages/SignIn';
 import './styles/main.scss';
 
 function App() {
-  return <div>div</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,12 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyDJZtfMJ97aFxbcs9TR-cP3LifAkuGJkd8',
   authDomain: 'chat-web-app-36298.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-36298-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'chat-web-app-36298',
   storageBucket: 'chat-web-app-36298.appspot.com',
   messagingSenderId: '176117343307',
@@ -10,3 +14,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
